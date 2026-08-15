443ddac15ce000a8a53e19dec3cbaa5576574b09
Zy0x <zy0x.noir@gmail.com>
2026-08-16 03:39:14 +0800

    chore(cleanup): remove Buka_AutoGram.bat production launcher

--- Buka_AutoGram.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-title AutoGram Desktop
-set WEBVIEW2_ADDITIONAL_BROWSER_ARGUMENTS=--remote-debugging-port=9230
-cd /d "f:\AutoGram\AutoGram App\frontend\src-tauri"
-start "" "target\release\frontend.exe"
-exit
-
